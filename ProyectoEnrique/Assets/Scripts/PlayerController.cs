92e96742977638b839fcf4150d46144327fe674a
Enrique Suarez <enriquesuarezgtte@gmail.com>
2019-08-31 01:54:29 -0500

    Rollaball parcial

--- ProyectoEnrique/Assets/Scripts/PlayerController.cs
+++ ProyectoEnrique/Assets/Scripts/PlayerController.cs
@@ -6,24 +6,56 @@ using UnityEngine.UI;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
-    private int contador;
+    private string contador;
     private AudioSource audioRecoleccion;
     public Transform particulas;
     public Text textUI;
     public Text Award;
+    public GameObject cuboAzul;
     private ParticleSystem systemaParticulas;
     private Vector3 position;
+        private Vector3 positionAmarillo1;
+
+    private Vector3 positionAmarillo2;
+
+    private Vector3 positionAmarillo3;
+    private Vector3 posipositionRojo1;
+    private Vector3 posipositionRojo2;
+    private Vector3 posipositionRojo3;
+        private Vector3 posipositionAzul;
+
+
     public float speed;
+
+    public GameObject cuboAmarillo1;
+    public GameObject cuboAmarillo2;
+    public GameObject cuboAmarillo3;
+    public GameObject cuboRojo1;
+    public GameObject cuboRojo2;
+    public GameObject cuboRojo3;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         systemaParticulas = particulas.GetComponent<ParticleSystem>();
         systemaParticulas.Stop();
-        contador =0;
+        contador ="";
         Award.text ="";
+        
         audioRecoleccion=GetComponent<AudioSource>();
         textUI.text = "Contador: "+contador.ToString();
+
+
+        posipositionRojo1 = cuboRojo1.transform.position;
+        posipositionRojo2 = cuboRojo2.transform.position;
+        posipositionRojo3 = cuboRojo3.transform.position;
+
+        positionAmarillo1 = cuboAmarillo1.transform.position;
+        positionAmarillo2 = cuboAmarillo2.transform.position;
+        positionAmarillo3 = cuboAmarillo3.transform.position;
+        posipositionAzul = cuboAzul.transform.position;
+
+
     }
 
     // Update is called once per frame
@@ -45,25 +77,65 @@ public class PlayerController : MonoBehaviour
     }
        void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.CompareTag("Recolectable")){
+        if(other.gameObject.CompareTag("Amarillo")){
             Debug.Log(contador);
-            contador++;
-            textUI.text = "Contador: "+contador.ToString();
+            contador="Amarillo";
+            textUI.text = "Color: "+contador.ToString();
             position = other.gameObject.transform.position;
-            particulas.position=position;
-            systemaParticulas = particulas.GetComponent<ParticleSystem> ();
-            systemaParticulas.Play();
-            other.gameObject.SetActive(false);
+            //particulas.position=position;
+            //systemaParticulas = particulas.GetComponent<ParticleSystem> ();
+            //systemaParticulas.Play();
+            other.gameObject.transform.position = new Vector3(
+            other.gameObject.transform.position.x-1,
+            other.gameObject.transform.position.y,
+            other.gameObject.transform.position.z
+            );
+        
+            
             audioRecoleccion.Play();
-            if(contador>=12){
-            Award.text="Ganaste";
-            
-        }else{
-            
-             
-           // SceneManager.LoadScene(1);
 
-        }
+    }else if (other.gameObject.CompareTag("Rojo")){
+        Debug.Log(contador);
+            contador="Rojo";
+            textUI.text = "Color: "+contador.ToString();
+            position = other.gameObject.transform.position;
+ 
+            other.gameObject.transform.position = new Vector3(
+            other.gameObject.transform.position.x+1,
+            other.gameObject.transform.position.y,
+            other.gameObject.transform.position.z
+            );
+        
+            
+            audioRecoleccion.Play();
+
+    }else if (other.gameObject.CompareTag("Verde")){
+        Debug.Log(contador);
+            contador="Verde";
+            textUI.text = "Color: "+contador.ToString();
+            position = other.gameObject.transform.position;
+            
+            cuboAzul.gameObject.transform.position = new Vector3(
+            cuboAzul.gameObject.transform.position.x,
+            cuboAzul.gameObject.transform.position.y,
+            -cuboAzul.gameObject.transform.position.z
+            );
+        
+            
+            audioRecoleccion.Play();
+
+    }else if(other.gameObject.CompareTag("Azul")){
+        
+contador="Azul";
+            textUI.text = "Color: "+contador.ToString();
+       cuboRojo1.transform.position= posipositionRojo1 ;
+        cuboRojo2.transform.position=posipositionRojo2 ;
+     cuboRojo3.transform.position=posipositionRojo3 ;
+
+       cuboAmarillo1.transform.position =positionAmarillo1 ;
+        cuboAmarillo2.transform.position=positionAmarillo2 ;
+        cuboAmarillo3.transform.position=positionAmarillo3 ;
+       cuboAzul.transform.position= posipositionAzul ;
     }
     }
 }
