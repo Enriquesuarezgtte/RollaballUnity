83c7037a3c13ab8d7f807f038b7851716eb7c127
Enrique Suárez Guette <enriquesuarezgtte@gmail.com>
2019-09-24 13:38:04 -0500

    Tarea corutinas

--- ProyectoEnrique/Assets/Scripts/PlayerController.cs
+++ ProyectoEnrique/Assets/Scripts/PlayerController.cs
@@ -15,17 +15,28 @@ public class PlayerController : MonoBehaviour
     private Vector3 position;
     public float speed;
     public GameObject CuboMovible;
+    public GameObject Cubo10Segundos;
+    public GameObject Cubo5y3segundos;
+    private int contadorColor;
+    private bool recolected;
+    private Material material;
+
     // Start is called before the first frame update
     void Start()
     {
+        contadorColor=1;
         rb = GetComponent<Rigidbody>();
         systemaParticulas = particulas.GetComponent<ParticleSystem>();
         systemaParticulas.Stop();
         contador =0;
         Award.text ="";
+        recolected=false;
+        material=Cubo5y3segundos.GetComponent<Renderer>().material;
         audioRecoleccion=GetComponent<AudioSource>();
         textUI.text = "Contador: "+contador.ToString();
         StartCoroutine("Movimiento");
+        StartCoroutine("DesaparecerCubo");
+        StartCoroutine("DesaparecerCubo5y3");
     }
 
     // Update is called once per frame
@@ -47,6 +58,7 @@ public class PlayerController : MonoBehaviour
     }
        void OnTriggerEnter(Collider other)
     {
+
         if(other.gameObject.CompareTag("Recolectable")){
             Debug.Log(contador);
             contador++;
@@ -59,20 +71,72 @@ public class PlayerController : MonoBehaviour
 
             other.gameObject.SetActive(false);
             audioRecoleccion.Play();
+          
+                Debug.Log("RRR");
+                recolected=true;
+            
             if(contador>=13){
-            Award.text="Ganaste";
+            Award.text="Ganaste";            
+            }
+        
+       
+    }
+      if(other.gameObject.CompareTag("RecolectableR")){
+            Debug.Log("ASD");
+            contador++;
+            textUI.text = "Contador: "+contador.ToString();
+            position = other.gameObject.transform.position;
+            particulas.position=position;
+            systemaParticulas = particulas.GetComponent<ParticleSystem> ();
+            systemaParticulas.Play();
             
-        }else{
-            
+            StartCoroutine(DetenerParticulas(systemaParticulas));
+            other.gameObject.SetActive(false);
+            audioRecoleccion.Play();
+            recolected=true;
              
            // SceneManager.LoadScene(1);
 
         }
     }
-    }
     public IEnumerator DetenerParticulas(ParticleSystem part){
         yield return new WaitForSecondsRealtime(5);
         part.Stop();
+    }
+
+    public IEnumerator DesaparecerCubo(){
+        yield return new WaitForSecondsRealtime(10);
+        Cubo10Segundos.SetActive(false);
+    }
+    public IEnumerator DesaparecerCubo5y3(){
+        
+        for(;;){
+            if(!recolected){
+            if(Cubo5y3segundos.gameObject.activeSelf){
+                Cubo5y3segundos.SetActive(false);
+            yield return new WaitForSecondsRealtime(5);
+            }else if(!Cubo5y3segundos.gameObject.activeSelf){
+               
+            Cubo5y3segundos.SetActive(true);
+             if(contadorColor==1){
+                    Cubo5y3segundos.GetComponent<Renderer>().material.color = Color.green; 
+                }else if(contadorColor ==2){
+                    Cubo5y3segundos.GetComponent<Renderer>().material.color = Color.black; 
+                }else if(contadorColor ==3){
+                    Cubo5y3segundos.GetComponent<Renderer>().material.color = Color.red; 
+                }else if(contadorColor ==4){
+                    Cubo5y3segundos.GetComponent<Renderer>().material.color = Color.white; 
+                }else if(contadorColor ==5){
+                    Cubo5y3segundos.GetComponent<Renderer>().material.color = Color.blue; 
+                    contadorColor=0;
+                }
+                contadorColor++;
+            yield return new WaitForSecondsRealtime(3);
+            }
+            }else{
+                break;
+            }
+        }
     }
 
     public IEnumerator Movimiento() {
