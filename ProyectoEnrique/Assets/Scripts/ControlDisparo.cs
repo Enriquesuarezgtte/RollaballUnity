b33c526510402b6c76903dce8bb8428bfb6c22fa
Enrique Suarez <enriquesuarezgtte@gmail.com>
2019-10-05 01:51:45 -0500

    Disparo

---
+++ ProyectoEnrique/Assets/Scripts/ControlDisparo.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlDisparo : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    public GameObject Player;
+    public float TiempoEntreDisparos =1f;
+    public float rango =100f;
+
+    float timer;
+    Ray shootRay;
+    RaycastHit shootHit;
+    int shootableMask;
+    LineRenderer gunLine;
+    Light gunLight;
+    float effectsDisplayTime =1.2f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
