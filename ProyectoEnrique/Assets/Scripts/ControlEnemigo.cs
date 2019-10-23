7783139374cd3a43bae99da6b0c25f8a7d5f8456
Enrique Suarez <enriquesuarezgtte@gmail.com>
2019-10-22 21:26:18 -0500

    Enemy

---
+++ ProyectoEnrique/Assets/Scripts/ControlEnemigo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class ControlEnemigo : MonoBehaviour
+{
+
+    Transform posicionJugador;
+    NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        posicionJugador = GameObject.FindGameObjectWithTag("Jugador").transform;
+        agent = GetComponent<NavMeshAgent> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(posicionJugador.position);
+    }
+}
